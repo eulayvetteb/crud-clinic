08e8700fff0289900b709e40ddf0dfce4e68e792
eulayvetteb <eulayvetteb@gmail.com>
2025-12-18 00:35:14 +0800

    Add phone field to Doctor schema
    Added phone field to doctor schema.

--- models/Doctor.js
+++ models/Doctor.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const doctorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  specialty: { type: String, required: true }
-}, { timestamps: true });
+const doctorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    specialty: { type: String, required: true },
+    phone: { type: String, required: true }, // ✅ add phone
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Doctor', doctorSchema);
+module.exports = mongoose.model("Doctor", doctorSchema);
